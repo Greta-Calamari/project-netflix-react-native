8b3158e0d5ca4d1882f315320adef46c2d7ec5f7
ladykakki <greta_calamari@hotmail.it>
2022-09-27 10:35:58 +0200

    img fixed

--- components/MovieBox.tsx
+++ components/MovieBox.tsx
@@ -1,15 +1,38 @@
-import { View, Text } from 'react-native'
+import { View, Text ,Image,StyleSheet,ScrollView,} from 'react-native'
 import React from 'react'
-
-const MovieBox = ({title, poster_path,vote_average,relese_date,overview}:any) => {
+const API_img = "https://image.tmdb.org/t/p/w500/"
+const MovieBox = ({pagingEnabledtitle,title, poster_path,vote_average,release_date,overview}:any) => {
   return (
-    <View>
-      <Text style={{color:"white"}}>
+    <ScrollView  style={styles.scrollView}>
+      <Image 
+      style={styles.image}
+      source={{
+            uri: `https://image.tmdb.org/t/p/w500/${poster_path}`,
+        }}/>
+      {/* <Text style={{color:"white"}} >
         {title}
-        {poster_path}
       </Text>
-    </View>
+      <Text>
+        {release_date}
+      </Text> */}
+    </ScrollView>
+
+
+        
   )
 }
+const styles = StyleSheet.create({
+  
+  image:{
+    width:300,
+    height:300,
+
+  },
+  scrollView: {
+    marginHorizontal: 20,
+  },
+    
+  
+});
 
 export default MovieBox
