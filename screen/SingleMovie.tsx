2ddbb3f25d35b4ee3a7ef1ffa3c71d256d3c4fbf
ladykakki <greta_calamari@hotmail.it>
2022-09-28 16:43:02 +0200

    gradient

--- screen/SingleMovie.tsx
+++ screen/SingleMovie.tsx
@@ -1,7 +1,10 @@
-import { View, Text } from "react-native";
+import { View, Text, Image, StyleSheet, Dimensions } from "react-native";
 import React, { useEffect, useState } from "react";
 import { NativeStackHeaderProps } from "@react-navigation/native-stack";
 import { Continue } from "../types";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { MaterialIcons } from "@expo/vector-icons";
+import { LinearGradient } from "expo-linear-gradient";
 
 export default function SingleMovie({
   route,
@@ -35,7 +38,69 @@ export default function SingleMovie({
 
   return (
     <View>
-      <Text>{title}</Text>
+      {/* {title === title ? <Text>{title}</Text> : <Text>{name}</Text>} */}
+      <View style={styles.container}>
+        <Image
+          style={styles.image}
+          source={{
+            uri: `https://image.tmdb.org/t/p/w500/${poster_path}`,
+          }}
+        />
+        <LinearGradient
+          // Background Linear Gradient
+          colors={["rgba(0,0,0,0.8)", "transparent"]}
+          style={[
+            styles.background,
+            {
+              transform: [{ rotateY: "180deg" }, { rotateZ: "180deg" }],
+            },
+          ]}
+        />
+        <MaterialCommunityIcons
+          name="bookmark-minus-outline"
+          style={styles.book}
+        />
+        <MaterialIcons name="keyboard-arrow-left" style={styles.chevron} />
+      </View>
     </View>
   );
 }
+// styles
+
+//full width
+let width = Dimensions.get("window").width;
+
+//full height
+let height = Dimensions.get("window").height;
+
+const styles = StyleSheet.create({
+  image: {
+    width: width,
+    height: 600,
+  },
+  container: {
+    position: "relative",
+  },
+
+  book: {
+    position: "absolute",
+    right: 20,
+    top: 30,
+    color: "white",
+    fontSize: 30,
+  },
+  chevron: {
+    position: "absolute",
+    left: 20,
+    top: 30,
+    color: "white",
+    fontSize: 30,
+  },
+  background: {
+    position: "absolute",
+    left: 0,
+    right: 0,
+    bottom: 0,
+    height: 400,
+  },
+});
